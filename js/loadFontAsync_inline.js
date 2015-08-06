dc9557ffdcba3887294dfb3d8318afc3a23435d8
Ranuka Perera <random@sawrc.com>
2015-08-06 08:27:14 +0530

    Update 2015-08-06T02:54:34.017Z

---
+++ js/loadFontAsync_inline.js
@@ -0,0 +1,11 @@
+      WebFontConfig = {
+          google: {
+              families: ['Open+Sans:400,700:latin']
+          }
+      };
+
+      (function (d) {
+          var wf = d.createElement('script'), s = d.scripts[0];
+          wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+          s.parentNode.insertBefore(wf, s);
+      })(document);
